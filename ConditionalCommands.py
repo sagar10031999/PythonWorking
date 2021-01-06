847cd169797845a6949ca6eec07bb36cc4dc3006
sagar10031999 <60805975+sagar10031999@users.noreply.github.com>
2021-01-06 16:52:01 +0530

    Conditional Commands

---
+++ ConditionalCommands.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+#driver = webdriver.Chrome(executable_path="C:\DRIVERS\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Firefox(executable_path="C:\DRIVERS\geckodriver-v0.28.0-win64\geckodriver.exe")
