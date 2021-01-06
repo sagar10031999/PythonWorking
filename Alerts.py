186adc3c8b93d3ac29e3cea2f77c925989d445bb
sagar10031999 <60805975+sagar10031999@users.noreply.github.com>
2021-01-06 16:53:11 +0530

    Alerts

---
+++ Alerts.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+driver = webdriver.Chrome(executable_path="C:\DRIVERS\chromedriver_win32\chromedriver.exe")
+driver.get("https://testautomationpractice.blogspot.com/")
+driver.find_element_by_xpath("//*[@id='HTML9']/div[1]/button").click()
+time.sleep(5)
+driver.switch_to_alert().accept()
+driver.switch_to_alert().dismiss()
